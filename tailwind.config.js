0fcaa96c36588616f7036cbcfcc7519307bc4da7
nhimc9x <nhicomputer77@gmail.com>
2024-10-09 13:00:46 +0700

    Base Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#356C07'
+        'ct-primary': '#356C07',
+        'ct-secondary': '#68C114',
+        'ct-cta-color': '#E10000',
+        'ct-heading-color': '#1A1A1A',
+        'ct-text-color': '#4F4F4F',
+        'ct-sub-text-color': '#4F4F4F99',
       },
     },
   },
   plugins: [],
 }
-
